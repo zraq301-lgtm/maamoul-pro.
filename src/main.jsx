eb29569efdc2945e74572f2a661440a6b25c667b
zraq301-lgtm <zraq301@gmail.com>
2026-05-26 05:58:08 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import App from "./App.jsx";
 import LoginPage from "./LoginPage.jsx";
 import "./index.css";
 import "./App.css";
-
-// مكوّن حماية المسارات
-const ProtectedRoute = ({ children }) => {
-  // التحقق من حالة تسجيل الدخول
-  const isAuthenticated = localStorage.getItem("sb-access-token");
-  
-  if (!isAuthenticated) {
-    // إذا لم يكن مسجلاً، نرسله لصفحة الدخول
-    return <Navigate to="/login" replace />;
-  }
-  
-  return children;
-};
 
 // استخدام createRoot بشكل سليم
 const rootElement = document.getElementById("root");
@@ -27,20 +13,10 @@ ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* مسار تسجيل الدخول */}
-        <Route path="/login" element={<LoginPage />} />
+        {/* صفحة تسجيل الدخول هي المسار الرئيسي الآن */}
+        <Route path="/" element={<LoginPage />} />
         
-        {/* المسارات المحمية */}
-        <Route 
-          path="/*" 
-          element={
-            <ProtectedRoute>
-              <App />
-            </ProtectedRoute>
-          } 
-        />
-        
-        {/* إعادة توجيه أي مسار غير موجود للرئيسية */}
+        {/* أي مسار آخر يتم توجيهه لصفحة الدخول */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
